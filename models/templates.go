a4f28210d75a06da7c24568da91748bf2bcd7d4d
Johann Gnaucke <johann.gnaucke@plusserver.com>
2021-09-11 07:26:55 +0200

    Added name and description to templates

--- models/templates.go
+++ models/templates.go
@@ -3,9 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Template struct {
-	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	ItemIDs    []primitive.ObjectID
-	Items      []Item `bson:"-"`
-	TriggerIDs []primitive.ObjectID
-	Triggers   []Trigger `bson:"-"`
+	ID                primitive.ObjectID `bson:"_id,omitempty"`
+	Name, Description string
+	ItemIDs           []primitive.ObjectID
+	Items             []Item `bson:"-"`
+	TriggerIDs        []primitive.ObjectID
+	Triggers          []Trigger `bson:"-"`
 }
